e75416306318d727344a12811c97fc091d8520c3
rabiaculha <rabiaculha3424@gmail.com>
2025-12-11 21:51:58 +0300

    Add files via upload

---
+++ donguler.lab1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    //1 den 100 e kadar olan sayılar
+    int i;
+    for(i=1;i<=100;i++){
+        printf("%d",i);
+    }
+    return 0;
+}
